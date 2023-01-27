1c9f72efc6eca95447e9b29f782c26736ef48dec
Andrew Morell <ammorell13@hotmail.com>
2023-01-27 14:11:02 -0700

    restored RW preset evaluation to original ordering

--- src/utilities/simIncludeRW.py
+++ src/utilities/simIncludeRW.py
@@ -91,22 +91,6 @@ class rwFactory(object):
         # create the blank RW object
         RW = messaging.RWConfigMsgPayload()
 
-        if 'maxMomentum' in kwargs:
-            varMaxMomentum = kwargs['maxMomentum']
-            if not isinstance(varMaxMomentum, float):
-                print('ERROR: maxMomentum must be a FLOAT argument')
-                exit(1)
-        else:
-            varMaxMomentum = 0.0              # default value
-        self.maxMomentum = varMaxMomentum
-
-        # populate the RW object with the type specific parameters
-        try:
-            eval('self.' + rwType + '(RW)')
-        except:
-            print('ERROR: RW type ' + rwType + ' is not implemented')
-            exit(1)
-
         # process optional input arguments
         if 'RWModel' in kwargs:
             varRWModel =  kwargs['RWModel']
@@ -139,6 +123,15 @@ class rwFactory(object):
                 exit(1)
         else:
             varUseMaxTorque = True              # default value
+
+        if 'maxMomentum' in kwargs:
+            varMaxMomentum = kwargs['maxMomentum']
+            if not isinstance(varMaxMomentum, float):
+                print('ERROR: maxMomentum must be a FLOAT argument')
+                exit(1)
+        else:
+            varMaxMomentum = 0.0              # default value
+        self.maxMomentum = varMaxMomentum
 
         if 'maxPower' in kwargs:
             varMaxPower = kwargs['maxPower']
@@ -200,6 +193,13 @@ class rwFactory(object):
         else:
             varLabel = 'RW' + str(len(self.rwList)+1)        # default device labeling
         RW.label = varLabel
+
+        # populate the RW object with the type specific parameters
+        try:
+            eval('self.' + rwType + '(RW)')
+        except:
+            print('ERROR: RW type ' + rwType + ' is not implemented')
+            exit(1)
 
         if 'u_min' in kwargs:
             varu_min = kwargs['u_min']
